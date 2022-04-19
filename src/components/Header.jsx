b32771137d8313333b93aff7932776ab5becf9c1
bobycat22 <anthonyznj@gmail.com>
2022-04-19 17:16:28 -0400

    minor updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,25 @@
-import '../assets/css/Header.css';
-import Logo from '../assets/imgs/logo.png';
+import "../assets/css/Header.css";
+import Logo from "../assets/imgs/logo.png";
 
 function Header() {
-    return (
-        <div className="head">
-            <div>
-                <div className="logo">
-                    <img src={Logo} alt="logo" size="200" height="200" className="logo"></img>
-                </div>
-                <h1 className="title">MontyHacks V</h1>
-                <br />
-                <h3 className="hackathon-date">June 11th</h3>
-            </div>
+  return (
+    <div className="head">
+      <div>
+        <div className="logo">
+          <img
+            src={Logo}
+            alt="logo"
+            size="250"
+            height="250"
+            className="logo"
+          ></img>
         </div>
-    )
+        <h1 className="title">MontyHacks V</h1>
+        <br />
+        <h3 className="hackathon-date">June 11th</h3>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
