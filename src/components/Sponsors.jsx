b3820f7f8f9467a5b5d04b8639e860055e7a7e81
Anthony <anthonyznj@gmail.com>
2022-04-21 15:17:21 -0400

    add pediatric

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -1,4 +1,5 @@
 import "../assets/css/Sponsors.scss";
+import Pediatric from "../assets/sponsors/pediatric.jpg";
 
 function Sponsors() {
   return (
@@ -6,7 +7,13 @@ function Sponsors() {
       <div style={{ height: "70px" }}></div>
       <div className="sponsors">
         <h2 className="section-title">sponsors</h2>
-        <p className="sponsors-text">Coming soon...</p>
+        <div
+        id= "sponsor_logos">
+          <img
+            src={Pediatric}
+            alt="pediatric logo"
+          />
+        </div>
       </div>
     </div>
   );
