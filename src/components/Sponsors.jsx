47b7430bda76490c1848235d3c1cd36142877808
bobycat22 <anthonyznj@gmail.com>
2022-05-05 21:33:29 -0400

    add echo3d and 1password

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -10,6 +10,8 @@ import Roboflow from "../assets/sponsors/roboflow2.png";
 import Voiceflow from "../assets/sponsors/voiceflow.png";
 import Wolfram from "../assets/sponsors/wolfram.svg";
 import XYZ from "../assets/sponsors/xyz.png";
+import OnePassword from "../assets/sponsors/1password.png";
+import echo3d from "../assets/sponsors/echo3d.png";
 
 function Sponsors() {
   return (
@@ -19,7 +21,11 @@ function Sponsors() {
       <div className="sponsors">
         <div className="plat-sponsor-logos"></div>
         <div className="gold-sponsor-logos">
-          <img src={Pediatric} alt="pediatric logo" className="logo" />
+          <img
+            src={Pediatric}
+            alt="pediatric logo"
+            className="financial-logo"
+          />
         </div>
         <div className="silver-sponsor-logos">
           <div className="silver-row">
@@ -27,43 +33,76 @@ function Sponsors() {
             <img
               src={CodeNinjas}
               alt="code ninjas logo"
-              className="logo"
+              className="financial-logo"
               id="codeninjas"
             />
-            <img src={TigersTale} alt="tigerstale logo" className="logo" />
+            <img
+              src={TigersTale}
+              alt="tigerstale logo"
+              className="financial-logo"
+            />
           </div>
         </div>
         <div style={{ height: "70px" }}></div>
         <h2 className="section-title">in-kind sponsors</h2>
+        <div style={{ height: "20px" }}></div>
         <div className="in-kind-logos">
           <div className="in-kind-row">
-            <img
-              src={Roboflow}
-              alt="roboflow logo"
-              className="logo"
-              id="roboflow"
-            />
-            <img
-              src={InterviewCake}
-              alt="interview cake logo"
-              className="logo"
-              id="interviewcake"
-            />
-            <img
-              src={Voiceflow}
-              alt="voiceflow logo"
-              className="logo"
-              id="voiceflow"
-            />
+            <div>
+              <img
+                src={Roboflow}
+                alt="roboflow logo"
+                className="logo"
+                id="roboflow"
+              />
+            </div>
+            <div>
+              <img
+                src={InterviewCake}
+                alt="interview cake logo"
+                className="logo"
+                id="interviewcake"
+              />
+            </div>
+            <div>
+              <img
+                src={Voiceflow}
+                alt="voiceflow logo"
+                className="logo"
+                id="voiceflow"
+              />
+            </div>
           </div>
           <div className="in-kind-row">
-            <img
-              src={Wolfram}
-              alt="wolfram logo"
-              className="logo"
-              id="wolfram"
-            />
-            <img src={XYZ} alt="xyz logo" className="logo" id="xyz" />
+            <div>
+              <img
+                src={Wolfram}
+                alt="wolfram logo"
+                className="logo"
+                id="wolfram"
+              />
+            </div>
+            <div>
+              <img src={XYZ} alt="xyz logo" className="logo" id="xyz" />
+            </div>
+            <div>
+              <img
+                src={OnePassword}
+                alt="1password logo"
+                className="logo"
+                id="onepassword"
+              />
+            </div>
+          </div>
+          <div className="in-kind-row">
+            <div>
+              <img
+                src={echo3d}
+                alt="echo3d logo"
+                className="logo"
+                id="echo3d"
+              />
+            </div>
           </div>
         </div>
       </div>
